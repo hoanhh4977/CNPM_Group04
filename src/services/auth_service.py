b62f28c3665815e866c432eba9254f16b2492a10
hoanhh4977 <hoanhhbmt49@gmail.com>
2025-11-12 21:20:25 +0700

    tạo class cho chức năng đăng ký/đăng nhập

---
+++ src/services/auth_service.py
@@ -0,0 +1,11 @@
+from src.storage.repositories import user_repository
+
+class AuthService:
+    def __init__(self):
+        pass
+    
+    def login(self):
+        pass
+    
+    def register(self):
+        pass
