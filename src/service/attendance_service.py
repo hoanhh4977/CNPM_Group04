2994461beee38383fb7cfd17cdc81deae779be30
mai linh <batmannani.123456789@gmail.com>
2025-11-15 01:46:52 +0700

    Update attendance_service.py

--- src/service/attendance_service.py
+++ src/service/attendance_service.py
@@ -1,28 +1,31 @@
-from datetime import datetime
 from src.repository.attendance_repository import AttendanceRepository
+from datetime import datetime
 
 class AttendanceService:
     def __init__(self):
         self.attendance_repo = AttendanceRepository()
 
-    def insert_attendance(self, student_id, session_id):
+    def insert_attendance(self, student_id: str, session_id: str):
         attendance_id = str(int(datetime.now().timestamp()))
-        data = {
+        attendance_data = {
             "attendance_id": attendance_id,
             "student_id": student_id,
             "session_id": session_id,
             "check_in_time": datetime.now().isoformat(),
             "status": "Present"
         }
-        self.attendance_repo.insert_attendance(data)
+        self.attendance_repo.table.insert(attendance_data).execute()
         return attendance_id
 
-    def get_attendance_by_student(self, student_id):
-        return self.attendance_repo.get_by_student(student_id).data
+    def get_attendance_by_student(self, student_id: str):
+        data = self.attendance_repo.table.select("*").eq("student_id", student_id).execute().data
+        return data if data else []
 
-    def get_attendance_by_session(self, session_id):
-        return self.attendance_repo.get_by_session(session_id).data
+    def get_attendance_by_session(self, session_id: str):
+        data = self.attendance_repo.table.select("*").eq("session_id", session_id).execute().data
+        return data if data else []
 
-    def update_attendance_status(self, attendance_id, new_status):
-        self.attendance_repo.update_status(attendance_id, new_status)
+    def update_attendance_status(self, attendance_id: str, new_status: str):
+        self.attendance_repo.table.update({"status": new_status})\
+            .eq("attendance_id", attendance_id).execute()
         return "✅ Đã cập nhật trạng thái điểm danh."
