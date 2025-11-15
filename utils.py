0015678bc892863b15e8434d9f3b97cd7324b7dd
mai linh <batmannani.123456789@gmail.com>
2025-11-16 00:19:35 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import random
+import string
 
-def generate_attendance_code():
-    return str(random.randint(10000, 99999))
+def generate_attendance_code(length=6):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
