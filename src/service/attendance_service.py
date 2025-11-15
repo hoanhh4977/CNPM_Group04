e8630365777e30610b80c8609d5a1fca0b330688
mai linh <batmannani.123456789@gmail.com>
2025-11-15 23:26:23 +0700

    Update attendance_service.py

--- src/service/attendance_service.py
+++ src/service/attendance_service.py
@@ -1,29 +1,8 @@
-import uuid
-from datetime import date
 from src.repository.attendance_repository import AttendanceRepository
 
 class AttendanceService:
     def __init__(self):
-        self.attendance_repository = AttendanceRepository()
+        self.attendance_repo = AttendanceRepository()
 
-    def get_attendance(self, student_id, session_id):
-        return self.attendance_repository.get_attendance(student_id, session_id)
-
-    def create_attendance(self, student_id, session_id, status):
-        attendance_id = uuid.uuid4().int % 1000000
-        attendance_data = {
-            "student_id": student_id,
-            "session_id": session_id,
-            "status": status
-        }
-        return self.attendance_repository.create_attendance(attendance_data)
-
-    def update_attendance(self, student_id, session_id, status):
-        update_data = {
-            "status": status
-        }
-        return self.attendance_repository.update_attendance(student_id, session_id, update_data)
-
-    def delete_attendance(self, student_id, session_id):
-        return self.attendance_repository.delete_attendance(student_id, session_id)
-    
+    def delete_attendance_by_id(self, attendance_id):
+        return self.attendance_repo.delete_attendance_by_id(attendance_id)
