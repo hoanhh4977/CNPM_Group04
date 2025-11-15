7172892444623269109c102e5b7f720a5b9dea6f
mai linh <batmannani.123456789@gmail.com>
2025-11-15 11:15:29 +0700

    Update attendance_service.py

--- src/service/attendance_service.py
+++ src/service/attendance_service.py
@@ -1,31 +1,28 @@
+import uuid
+from datetime import date
 from src.repository.attendance_repository import AttendanceRepository
-from datetime import datetime
 
 class AttendanceService:
     def __init__(self):
-        self.attendance_repo = AttendanceRepository()
+        self.attendance_repository = AttendanceRepository()
 
-    def insert_attendance(self, student_id: str, session_id: str):
-        attendance_id = str(int(datetime.now().timestamp()))
+    def get_attendance(self, student_id, session_id):
+        return self.attendance_repository.get_attendance(student_id, session_id)
+
+    def create_attendance(self, student_id, session_id, status):
+        attendance_id = uuid.uuid4().int % 1000000
         attendance_data = {
-            "attendance_id": attendance_id,
             "student_id": student_id,
             "session_id": session_id,
-            "check_in_time": datetime.now().isoformat(),
-            "status": "Present"
+            "status": status
         }
-        self.attendance_repo.table.insert(attendance_data).execute()
-        return attendance_id
+        return self.attendance_repository.create_attendance(attendance_data)
 
-    def get_attendance_by_student(self, student_id: str):
-        data = self.attendance_repo.table.select("*").eq("student_id", student_id).execute().data
-        return data if data else []
+    def update_attendance(self, student_id, session_id, status):
+        update_data = {
+            "status": status
+        }
+        return self.attendance_repository.update_attendance(student_id, session_id, update_data)
 
-    def get_attendance_by_session(self, session_id: str):
-        data = self.attendance_repo.table.select("*").eq("session_id", session_id).execute().data
-        return data if data else []
-
-    def update_attendance_status(self, attendance_id: str, new_status: str):
-        self.attendance_repo.table.update({"status": new_status})\
-            .eq("attendance_id", attendance_id).execute()
-        return "✅ Đã cập nhật trạng thái điểm danh."
+    def delete_attendance(self, student_id, session_id):
+        return self.attendance_repository.delete_attendance(student_id, session_id)
